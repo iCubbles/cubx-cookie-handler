9d1980198b8f82444dda584bfd567a0bc6a08dd9
edwingamboa <edwingamboa355@gmail.com>
2017-11-06 10:14:07 -0500

    CUBBLES-67: Eslint fix

--- webpackages/cubx-cookie-handler/cubx-cookie-reader/cubx-cookie-reader.js
+++ webpackages/cubx-cookie-handler/cubx-cookie-reader/cubx-cookie-reader.js
@@ -61,7 +61,7 @@
       return {
         key: this._extractKeyFromCookieString(cookieString),
         value: this._extractValueFromCookieString(cookieString)
-      }
+      };
     },
 
     _extractValueFromCookieString: function (cookieString) {
